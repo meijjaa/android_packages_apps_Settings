2dbd0c69ab7607b1e9347613a86805ccbf32d5d9
Chih-Wei Huang <cwhuang@linux.org.tw>
2015-10-02 01:01:48 +0800

    Add an option to control native bridge

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -50,6 +50,7 @@ public class Settings extends SettingsActivity {
     public static class DisplaySettingsActivity extends SettingsActivity { /* empty */ }
     public static class DeviceInfoSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ApplicationSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class AppsCompatibilityActivity extends SettingsActivity { /* empty */ }
     public static class ManageApplicationsActivity extends SettingsActivity { /* empty */ }
     public static class ManageAssistActivity extends SettingsActivity { /* empty */ }
     public static class AllApplicationsActivity extends SettingsActivity { /* empty */ }
